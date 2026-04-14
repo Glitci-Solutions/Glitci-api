05ae6d5cd40318fe94573e5ace503f7213738584
Ahmed Magdy <76597137+Ahmed-Magdyyy@users.noreply.github.com>
2026-04-14 17:35:45 +0200

    bug fix

--- src/modules/users/user.routes.js
+++ src/modules/users/user.routes.js
@@ -30,6 +30,10 @@ const router = Router();
 router.get("/me", protect, getMe);
 router.patch(
   "/me",
+  (req, res, next) => {
+    console.log(req.body);
+    next();
+  },
   protect,
   uploadSingleImage("image"),
   updateMeValidator,
