9779f816cfbe5e7a4cde29a8a46e6fc9825cd666
Ahmed Magdy <76597137+Ahmed-Magdyyy@users.noreply.github.com>
2026-04-16 11:17:08 +0200

    bug fix

--- src/shared/utils/createToken.js
+++ src/shared/utils/createToken.js
@@ -3,13 +3,15 @@ import jwt from "jsonwebtoken";
 // Access Token (short-lived 3H)
 export const createAccessToken = (userId, role) => {
   return jwt.sign({ userId, role }, process.env.JWT_ACCESS_SECRET, {
-    expiresIn: "3h",
+    // expiresIn: "3h",
+    expiresIn: "1m",
   });
 };
 
 // Refresh Token (long-lived 30D)
 export const createRefreshToken = (userId) => {
   return jwt.sign({ userId }, process.env.JWT_REFRESH_SECRET, {
-    expiresIn: "30d",
+    // expiresIn: "30d",
+    expiresIn: "3m",
   });
 };
